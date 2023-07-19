91dac02f0a0c3c5f24c585a02bb4667b9a888f34
Shirerpeton <hyloxalusfallax@gmail.com>
2023-07-19 22:36:57 +0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿if(args.Length > 0) {
+    Console.WriteLine("Usage: sclox [script]");
+    return;
+} else if(args.Length == 1) {
+    RunFile(args[0]);
+} else {
+    RunPrompt();
+}
+
+static void RunFile(string path) {
+    if(!File.Exists(path)) {
+        Console.WriteLine("File doesn't exist!");
+    }
+    string content = File.ReadAllText(path);
+    //Run(content);
+}
+
+static void RunPrompt() {
+    while(true) {
+        Console.Write("> ");
+        string? line = Console.ReadLine();
+        if(string.IsNullOrEmpty(line)) {
+            break;
+        }
+        //run(line);
+    }
+}
