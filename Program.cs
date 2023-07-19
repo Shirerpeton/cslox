df8616151ff0ac84918dd731cd19fe1aea8b303d
Shirerpeton <hyloxalusfallax@gmail.com>
2023-07-19 22:59:16 +0400

    add rudimentary error handling

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿namespace cslox;
 static class Program {
+    static bool hadError = false;
     static void Main(string[] args){
         if(args.Length > 0) {
             Console.WriteLine("Usage: sclox [script]");
+            Environment.ExitCode = 64;
             return;
         } else if(args.Length == 1) {
             RunFile(args[0]);
@@ -14,9 +16,14 @@ static class Program {
     static void RunFile(string path) {
         if(!File.Exists(path)) {
             Console.WriteLine("File doesn't exist!");
+            Environment.ExitCode = 64;
+            return;
         }
         string content = File.ReadAllText(path);
         Run(content);
+        if(hadError) {
+            Environment.ExitCode = 65;
+        }
     }
 
     static void RunPrompt() {
@@ -27,6 +34,9 @@ static class Program {
                 break;
             }
             Run(line);
+            if(hadError) {
+                hadError = false;
+            }
         }
     }
 
@@ -37,5 +47,14 @@ static class Program {
         foreach(Token token in tokens) {
             Console.WriteLine(token);
         }
+    }
+
+    static void Error(int line, string message) {
+        Report(line, "", message);
+        hadError = true;
+    }
+
+    static void Report(int line, string where, string message) {
+        Console.WriteLine($"[line {line} Error {where}: {message}");
     }
 }
