27c65ab1d19b5d5025bca07e41c487d207f15090
Shirerpeton <hyloxalusfallax@gmail.com>
2023-07-22 14:34:42 +0400

    add reverse polish notation printer for ast

--- AstRPNPrinter.cs
+++ AstRPNPrinter.cs
@@ -2,26 +2,25 @@ using System.Text;
 
 namespace cslox;
 
-public class AstPrinter: IVisitor<String> {
+public class AstRPNPrinter: IVisitor<String> {
     public string Print(Expr expr) {
         return expr.Accept(this);
     }
-    string Parenthesize(string name, Expr[] exprs) {
+    string RPNPrint(string name, Expr[] exprs) {
         var builder = new StringBuilder();
-        builder.Append("(").Append(name);
         foreach(Expr expr in exprs) {
-            builder.Append(" ");
             builder.Append(expr.Accept(this));
+            builder.Append(" ");
         }
-        builder.Append(")");
+        builder.Append(name);
         return builder.ToString();
     }
 
     public string VisitBinaryExpr(Binary expr) {
-        return Parenthesize(expr.opr.lexeme, new Expr[] { expr.left, expr.right });
+        return RPNPrint(expr.opr.lexeme, new Expr[] { expr.left, expr.right });
     } 
     public string VisitGroupingExpr(Grouping expr) {
-        return Parenthesize("group", new Expr[] { expr.expression });
+        return RPNPrint("group", new Expr[] { expr.expression });
     } 
     public string VisitLiteralExpr(Literal expr) {
         if(expr.value == null) {
@@ -30,6 +29,6 @@ public class AstPrinter: IVisitor<String> {
         return expr.value.ToString() ?? string.Empty;
     } 
     public string VisitUnaryExpr(Unary expr) {
-        return Parenthesize(expr.opr.lexeme, new Expr[] { expr.right });
+        return RPNPrint(expr.opr.lexeme, new Expr[] { expr.right });
     } 
 }
